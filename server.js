b5e2a83ab75b0e31e914574083b434a9f543d69a
yashrawat-lanet <yashrawat.lanet@gmail.com>
2020-10-26 15:18:48 +0530

    add validation of product id

--- server.js
+++ server.js
@@ -10,142 +10,161 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post('/viewDetail', (req, res) => {
-	MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
-		if (err) throw err;
-		var dbo = db.db('test');
-		switch (req.body.viewType) {
-			case 'week': {
-				dbo.collection('test')
-					.aggregate([
-						{ $match: { ProductId: req.body.ProductId } },
-						{ $project: { ViewDateWeek: { $week: '$ViewDate' }, UserId: '$UserId' } },
-						{
-							$group: {
-								_id: '$ViewDateWeek',
-								count: { $sum: 1 },
-								uniqueUsers: { $addToSet: '$UserId' },
-								UserId: { $first: '$UserId' },
-								users: { $push: '$UserId' },
-								WeekValue: { $first: '$ViewDateWeek' },
+	if (req.body.viewType) {
+		MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+			if (err) throw err;
+			var dbo = db.db('userView');
+			switch (req.body.viewType) {
+				case 'week': {
+					dbo.collection('userView')
+						.aggregate([
+							{ $match: { ProductId: req.body.ProductId } },
+							{ $project: { ViewDateWeek: { $week: '$ViewDate' }, UserId: '$UserId' } },
+							{
+								$group: {
+									_id: '$ViewDateWeek',
+									count: { $sum: 1 },
+									uniqueUsers: { $addToSet: '$UserId' },
+									UserId: { $first: '$UserId' },
+									users: { $push: '$UserId' },
+									WeekValue: { $first: '$ViewDateWeek' },
+								},
 							},
-						},
-						{
-							$addFields: { uniqueUserCount: { $size: '$uniqueUsers' }, totalUser: { $size: '$users' } },
-						},
-					])
-					.toArray(function (err, result) {
-						if (err) throw err;
-						res.send(result);
-						db.close();
-					});
-				break;
-			}
-			case 'month': {
-				dbo.collection('test')
-					.aggregate([
-						{ $match: { ProductId: req.body.ProductId } },
-						{ $project: { ViewDateWeek: { $month: '$ViewDate' }, UserId: '$UserId' } },
-						{
-							$group: {
-								_id: '$ViewDateWeek',
-								count: { $sum: 1 },
-								uniqueUsers: { $addToSet: '$UserId' },
-								UserId: { $first: '$UserId' },
-								users: { $push: '$UserId' },
-								MonthValue: { $first: '$ViewDateWeek' },
+							{
+								$addFields: {
+									uniqueUserCount: { $size: '$uniqueUsers' },
+									totalUser: { $size: '$users' },
+								},
 							},
-						},
-						{
-							$addFields: { uniqueUserCount: { $size: '$uniqueUsers' }, totalUser: { $size: '$users' } },
-						},
-					])
-					.toArray(function (err, result) {
-						if (err) throw err;
-						res.send(result);
-						db.close();
-					});
-				break;
-			}
-			case 'custom': {
-				const startDate = req.body.startDate ? new Date(req.body.startDate) : new Date('0');
-				const endDate = req.body.endDate ? new Date(req.body.endDate) : new Date();
-				dbo.collection('test')
-					.aggregate([
-						{
-							$match: { ViewDate: { $gte: startDate, $lte: endDate }, ProductId: req.body.ProductId },
-						},
-						{ $project: { ProductId: '$ProductId', UserId: '$UserId' } },
-						{
-							$group: {
-								_id: '$ProductId',
-								viewCount: { $sum: 1 },
-								uniqueUsers: { $addToSet: '$UserId' },
-								users: { $push: '$UserId' },
+						])
+						.toArray(function (err, result) {
+							if (err) throw err;
+							db.close();
+							res.send(result);
+						});
+					break;
+				}
+				case 'month': {
+					dbo.collection('userView')
+						.aggregate([
+							{ $match: { ProductId: req.body.ProductId } },
+							{ $project: { ViewDateWeek: { $month: '$ViewDate' }, UserId: '$UserId' } },
+							{
+								$group: {
+									_id: '$ViewDateWeek',
+									count: { $sum: 1 },
+									uniqueUsers: { $addToSet: '$UserId' },
+									UserId: { $first: '$UserId' },
+									users: { $push: '$UserId' },
+									MonthValue: { $first: '$ViewDateWeek' },
+								},
 							},
-						},
-						{
-							$addFields: { uniqueUserCount: { $size: '$uniqueUsers' }, totalUser: { $size: '$users' } },
-						},
-					])
-					.toArray(function (err, result) {
-						if (err) throw err;
-						res.send(result);
-						db.close();
-					});
-				break;
-			}
-			case 'daily': {
-				dbo.collection('test')
-					.aggregate([
-						{ $match: { ProductId: req.body.ProductId } },
-						{
-							$group: {
-								_id: '$ViewDate',
-								count: { $sum: 1 },
-								uniqueUsers: { $addToSet: '$UserId' },
-								UserId: { $first: '$UserId' },
-								users: { $push: '$UserId' },
+							{
+								$addFields: {
+									uniqueUserCount: { $size: '$uniqueUsers' },
+									totalUser: { $size: '$users' },
+								},
 							},
-						},
-						{
-							$addFields: { uniqueUserCount: { $size: '$uniqueUsers' }, totalUser: { $size: '$users' } },
-						},
-					])
-					.toArray(function (err, result) {
-						if (err) throw err;
-						res.send(result);
-						db.close();
-					});
-				break;
-			}
-			case 'total': {
-				dbo.collection('test')
-					.aggregate([
-						{ $match: { ProductId: req.body.ProductId } },
-						{
-							$group: {
-								_id: '$ProductId',
-								viewCount: { $sum: 1 },
-								uniqueUsers: { $addToSet: '$UserId' },
-								users: { $push: '$UserId' },
+						])
+						.toArray(function (err, result) {
+							if (err) throw err;
+							db.close();
+							res.send(result);
+						});
+					break;
+				}
+				case 'custom': {
+					const startDate = req.body.startDate ? new Date(req.body.startDate) : new Date('0');
+					const endDate = req.body.endDate ? new Date(req.body.endDate) : new Date();
+					dbo.collection('userView')
+						.aggregate([
+							{
+								$match: { ViewDate: { $gte: startDate, $lte: endDate }, ProductId: req.body.ProductId },
 							},
-						},
-						{
-							$addFields: { uniqueUserCount: { $size: '$uniqueUsers' }, totalUser: { $size: '$users' } },
-						},
-					])
-					.toArray(function (err, result) {
-						if (err) throw err;
-						res.send(result);
-						db.close();
-					});
-				break;
+							{ $project: { ProductId: '$ProductId', UserId: '$UserId' } },
+							{
+								$group: {
+									_id: '$ProductId',
+									viewCount: { $sum: 1 },
+									uniqueUsers: { $addToSet: '$UserId' },
+									users: { $push: '$UserId' },
+								},
+							},
+							{
+								$addFields: {
+									uniqueUserCount: { $size: '$uniqueUsers' },
+									totalUser: { $size: '$users' },
+								},
+							},
+						])
+						.toArray(function (err, result) {
+							if (err) throw err;
+							db.close();
+							res.send(result);
+						});
+					break;
+				}
+				case 'daily': {
+					dbo.collection('userView')
+						.aggregate([
+							{ $match: { ProductId: req.body.ProductId } },
+							{
+								$group: {
+									_id: '$ViewDate',
+									count: { $sum: 1 },
+									uniqueUsers: { $addToSet: '$UserId' },
+									UserId: { $first: '$UserId' },
+									users: { $push: '$UserId' },
+								},
+							},
+							{
+								$addFields: {
+									uniqueUserCount: { $size: '$uniqueUsers' },
+									totalUser: { $size: '$users' },
+								},
+							},
+						])
+						.toArray(function (err, result) {
+							if (err) throw err;
+							db.close();
+							res.send(result);
+						});
+					break;
+				}
+				case 'total': {
+					dbo.collection('userView')
+						.aggregate([
+							{ $match: { ProductId: req.body.ProductId } },
+							{
+								$group: {
+									_id: '$ProductId',
+									viewCount: { $sum: 1 },
+									uniqueUsers: { $addToSet: '$UserId' },
+									users: { $push: '$UserId' },
+								},
+							},
+							{
+								$addFields: {
+									uniqueUserCount: { $size: '$uniqueUsers' },
+									totalUser: { $size: '$users' },
+								},
+							},
+						])
+						.toArray(function (err, result) {
+							if (err) throw err;
+							db.close();
+							res.send(result);
+						});
+					break;
+				}
+				default:
+					res.send(null);
+					break;
 			}
-			default:
-				res.send(null);
-				break;
-		}
-	});
+		});
+	} else {
+		res.send({});
+	}
 });
 
 app.listen(port, () => {
